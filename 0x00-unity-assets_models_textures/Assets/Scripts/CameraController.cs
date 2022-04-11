b9a138a72e05090f5cc6c7cc24d420b5906144db
mutantEris <mmires@live.com>
2022-04-11 16:26:49 -0500

    turning

--- 0x00-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -9,14 +9,20 @@ public class CameraController : MonoBehaviour
     // distance between camera and player
     public Vector3 cameraOffset;
     public float sensitivity;
+    public float rotateHorizontal;
+    public float rotateVertical;
 
+    void start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + cameraOffset;
-        float rotateHorizontal = Input.GetAxis ("Mouse X");
-        float rotateVertical = Input.GetAxis ("Mouse Y");
-        transform.RotateAround (player.transform.position, -Vector3.up, rotateHorizontal * sensitivity);
-        transform.RotateAround (Vector3.zero, transform.right, rotateVertical * sensitivity);
+        rotateHorizontal = Input.GetAxis("Mouse X");
+        rotateVertical = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotateVertical * sensitivity, rotateHorizontal * sensitivity, 0, Space.World);
+        gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 0);
+        transform.position = player.transform.position;
     }
 }
