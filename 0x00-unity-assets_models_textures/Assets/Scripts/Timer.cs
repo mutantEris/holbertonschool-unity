243da71788e7f59129a536983b6bd16287113dc7
mutantEris <mmires@live.com>
2022-05-05 12:24:53 -0500

    progress

--- 0x00-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public Text timerText;
+    public Text timerTime;
     public float start;
+    public GameObject PlayerPill;
 
     void Start()
     {
@@ -16,5 +17,41 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ((PlayerPill.transform.position.x != 0 | PlayerPill.transform.position.z != 0) && timerTime = 0.0)
+        {
+            timerGo();
+        }
     }
+        //timerStop();
+
+    // void characterMove()
+    // {
+    //     if (PlayerPill.transform.position.x != 0 | PlayerPill.transform.position.z != 0)
+    //     timerGo;
+    // }
+    void timerGo()
+    {
+        float time = Time.time - start;
+        string minutes = ((int) time / 60).ToString();
+        string seconds = (time % 60).ToString("f2");
+        timerTime.text = minutes + ":" + seconds;
+    
+    }
+    // public void timerStart()
+    // {
+    //     if (!GetComponent<Timer>().enabled)
+    //         if (PlayerPill.transform.position.x != 0 | PlayerPill.transform.position.z != 0)
+    //             GetComponent<Timer>().enabled = true;
+    //             timerGo();
+    // }
+    // void timerStop()
+    // {
+    //     if (time.GetComponent<Timer>().enabled)
+    //         time.GetComponent<Timer>().enabled = true;
+    // }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     other.GetComponent<Timer>;()
+    // }
 }
+
