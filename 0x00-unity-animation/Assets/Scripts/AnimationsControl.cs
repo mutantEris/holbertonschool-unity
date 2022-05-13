6e37dc0a21f039385c60a87c212cddfa6c170357
mutantEris <mmires@live.com>
2022-05-12 21:00:46 -0500

    animation is hard

--- 0x00-unity-animation/Assets/Scripts/AnimationsControl.cs
+++ 0x00-unity-animation/Assets/Scripts/AnimationsControl.cs
@@ -11,11 +11,41 @@ public class AnimationsControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerController playbert = player.GetComponent<PlayerController>();
+        if (playbert.isGrounded == false)
+        {
+            anim.SetBool("Jumping", true);
+            anim.SetBool("Falling", true);
+            anim.SetBool("Idling", false);
+        }
+        else
+        {
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", false);
+            anim.SetBool("Idling", true);
+        }
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     anim.SetBool("Jumping", true);
+        //     anim.SetBool("Running", false);
+        // }
+        // else 
+        // {
+        //     anim.SetBool("Jumping", false);
+        //     anim.SetBool("Running", true);
+        // }
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
             anim.SetBool("Running", true);
+            anim.SetBool("Idling", false);
+        }
         // else if (Input.GetAxisRaw("Horizontal"))
         //     anim.SetBool("Running", true);
-        else
-            anim.SetBool("Running", false);
+        
+        // else
+        // {
+        //     anim.SetBool("Running", false);
+        //     anim.SetBool("Idling", true);
+        // }
     }
 }
